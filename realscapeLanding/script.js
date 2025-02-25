2464c4a26e923775518f70875e6ac0a3b212671a
MaartenSlebos <maarten.slebos@hotmail.com>
2025-02-25 20:21:40 +0100

    funny xp message

---
+++ realscapeLanding/script.js
@@ -0,0 +1,3 @@
+document.getElementById("joinButton").addEventListener("click", function() {
+    document.getElementById("pointsDisplay").innerText = "You’ve earned 10 XP for starting your journey!";
+  });
